346efa7d7f9970600ecef807974e053e17272988
Claire <cbr.programming@gmail.com>
2024-04-18 12:29:22 -0400

    Counter components + Counter state and clickHandle with buttons

---
+++ src/components/Counter.jsx
@@ -0,0 +1,25 @@
+import React from "react";
+
+export default function Counter() {
+    let [counter, setCounter] = React.useState(0);
+
+    function handleClickIncrements() {
+        setCounter(counter + 1);
+    }
+
+    function handleClickDecrements() {
+        setCounter(counter - 1);
+    }
+
+    return (
+        <>
+        <button onClick={handleClickIncrements}>
+            Incremente
+        </button>
+            <button onClick={handleClickDecrements}>
+                Decremente
+            </button>
+        <h1>{counter}</h1>
+        </>
+    )
+}
